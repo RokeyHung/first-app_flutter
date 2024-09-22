bd42d55bb38dfd1054ec7db716a57b0173d4dab5
hung.ngomanh <hhungnm@gmail.com>
2024-09-22 13:30:20 +0700

    Margin, padding use Card widget

--- lib/main.dart
+++ lib/main.dart
@@ -6,30 +6,26 @@ void main() {
       theme: ThemeData(
         fontFamily: "Inter",
       ),
-      home: SafeArea(
+      home: const SafeArea(
           child: Scaffold(
         body: MyWidget(),
       ))));
 }
 
 class MyWidget extends StatelessWidget {
+  const MyWidget({super.key});
+
   @override
   Widget build(BuildContext context) {
-    return RichText(
-      text: TextSpan(
-          style: DefaultTextStyle.of(context).style,
-          children: <TextSpan>[
-            TextSpan(
-                style: TextStyle(
-                    color: Colors.pink,
-                    fontWeight: FontWeight.w700,
-                    fontSize: 20),
-                text:
-                    "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, "),
-            TextSpan(
-                text:
-                    "and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.")
-          ]),
-    );
+    return const Card(
+        color: Colors.pink,
+        margin: EdgeInsets.fromLTRB(0, 20, 0, 0),
+        child: Padding(
+          padding: EdgeInsets.symmetric(horizontal: 12, vertical: 10),
+          child: Text(
+            'Springfield',
+            style: TextStyle(fontSize: 20, color: Colors.black54),
+          ),
+        ));
   }
 }
